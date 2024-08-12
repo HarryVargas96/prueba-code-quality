bc6448fc47a0970900d3346afb92ca0cd353db1a
Harry Vargas <hvargasr@unal.edu.co>
2024-08-11 20:53:15 -0500

    add preprocessing

--- prueba/utils.py
+++ prueba/utils.py
@@ -27,3 +27,40 @@ def read_csv_file(file_path):
     except Exception as e:
         logging.error(f"An error occurred: {e}")
         return None
+    
+def process_and_save_as_parquet(df, output_parquet_path):
+    """
+    Processes the DataFrame by renaming columns and converting specified columns to categories,
+    then saves the resulting DataFrame as a Parquet file.
+
+    Parameters:
+    df (DataFrame): The input DataFrame to process.
+    output_parquet_path (str): The file path where the output Parquet file will be saved.
+
+    Returns:
+    DataFrame: The processed DataFrame.
+    """
+    # Define column names
+    nombres = [
+        'edad', 'trabajo', 'estado_civil', 'educacion', 'mora', 'vivienda', 
+        'consumo', 'contacto', 'mes', 'dia', 'campana', 'dias_ultima_camp', 
+        'no_contactos', 'resultado_anterior', 'emp_var_rate', 'cons_price_idx', 
+        'cons_conf_idx', 'euriobor3m', 'nr_employed', 'y'
+    ]
+    
+    # Rename the columns
+    df.columns = nombres
+    
+    # Convert specified columns to categories
+    categorical_columns = [
+        'trabajo', 'estado_civil', 'educacion', 'mora', 'vivienda', 
+        'consumo', 'contacto', 'mes', 'dia', 'resultado_anterior'
+    ]
+    df = df.astype(dtype={col: 'category' for col in categorical_columns})
+    
+    # Save the DataFrame as a Parquet file
+    df.to_parquet(output_parquet_path, index=False)
+    logging.info(f"Data saved to {output_parquet_path} successfully.")
+    
+    # Return the processed DataFrame
+    return df
