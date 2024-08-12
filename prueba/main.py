c4cd3b5acee638972c7ddf0348e1b49a163c754d
Harry Vargas <hvargasr@unal.edu.co>
2024-08-11 20:50:50 -0500

    first commit/home/hvarga/.cache/pypoetry/virtualenvs/prueba-eJ4qdxRC-py3.10/bin/python

--- prueba/main.py
+++ prueba/main.py
@@ -1,4 +1,4 @@
-from utils import read_csv_file
+from utils import read_csv_file, process_and_save_as_parquet
 import os
 import logging
 
@@ -14,5 +14,9 @@ logging.info(f"Current working directory: {current_directory}")
 file_path = "/home/hvarga/prueba/data/raw/train.csv"
 
 df = read_csv_file(file_path)
-if df is not None:
-    print(df.head())
+
+logging.info(f"Data shape {df.shape}")
+
+interim_data_path = "/home/hvarga/prueba/data/interim/train.parquet"
+df = process_and_save_as_parquet(df = df,output_parquet_path=interim_data_path)
+
